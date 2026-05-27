23aaf9b05fb8cd8317ea630ed52dd913601064b3
jovixu <jovicadufour@gmail.com>
2026-05-27 14:38:08 +0200

    (doc): add docstrings for functions in logreg_train.py

--- srcs/logreg_train.py
+++ srcs/logreg_train.py
@@ -9,6 +9,27 @@ SEED = 21
 EVAL_DATASET_SIZE = 0.2
 
 def split_dataset(X: np.ndarray, y: np.ndarray, eval_size: float) -> tuple[np.ndarray]:
+    """
+    Splits each of the two original datasets into, for each original:
+        - One training dataset
+        - One evaluating dataset
+    The split is operated according to the _eval_size_ proportion.
+    Example:
+        _eval_size_ = 0.2 -> The resulting evaluating datasets will consist
+        in 20% of the original datasets. Therefore, the resulting training datasets
+        will consist in the remaining 80% of the original datasets.
+    The split is made at random to ensure representativity in each resulting dataset.
+
+    Args:
+        X (np.ndarray): The original dataset to split, containing the labels.
+        y (np.ndarray): The original dataset to split containing the classes.
+        eval_size (float): The proportion of the resulting eval datasets, relatively to
+        the original datasets.
+
+    Returns:
+        X_train, y_train, X_eval, y_eval (tuple[np.ndarray]) : The training and evaluating
+        datasets retrieved from the two original datasets.
+    """
     if not 0 < EVAL_DATASET_SIZE < 1:
         raise Exception("split_dataset(): EVAL_DATASET_SIZE must be between 0 and 1.")
     m = X.shape[0]
@@ -25,6 +46,20 @@ def split_dataset(X: np.ndarray, y: np.ndarray, eval_size: float) -> tuple[np.nd
     
     
 def ft_normalize(X: np.ndarray, min_train: float, max_train:float) -> np.ndarray:
+    """
+    Normalizes the values contained in the array passed as an argument according to the
+    minimum and maximum values of the training dataset.
+    Adds 1e-20 to the result in order to avoid potential divisions by 0.
+    
+    Args:
+        X (np.ndarray): Array in which the values will be normalized;
+        min_train (float): Minimum value in the training array.
+        max_train (float): Maximum value in the training array.
+
+    Returns:
+        (np.ndarray): The resulting normalized array.
+
+    """
     return (X - min_train) / ((max_train - min_train) + 1e-20) 
 
 
@@ -41,7 +76,18 @@ def test(X_eval, y_eval, W, b) -> None:
     print(f"success rate: {(success / len(X_eval) * 100):.2f}%")
 
 
-def normalize_features(X_train, X_eval) -> tuple[np.ndarray]:
+def normalize_features(X_train: np.ndarray, X_eval: np.ndarray) -> tuple[np.ndarray]:
+    """
+    Applies ft_normalized to both the training and evaluating datasets.
+
+    Args:
+    X_train (np.ndarray): The training dataset to be normalized.
+    X_eval (np.ndarray): The evaluating dataset to be normalized.
+
+    Returns:
+    X_train, X_eval (tuple[np.ndarray]): A tuple containing the resulting
+    normalized datasets.
+    """
     min_train = X_train.min(axis=0)
     max_train = X_train.max(axis=0)
 
